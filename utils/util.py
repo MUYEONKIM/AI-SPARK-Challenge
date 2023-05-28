65b68cae7273a444abcd1d6037e0e714dea541a1
MUYEONKIM <114132468+MUYEONKIM@users.noreply.github.com>
2023-05-28 22:35:36 +0900

    (Directory) run update, util update

---
+++ utils/util.py
@@ -0,0 +1,36 @@
+import json
+from collections import OrderedDict
+from itertools import repeat
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import torch
+
+
+def ensure_dir(dirname):
+    dirname = Path(dirname)
+    if not dirname.is_dir():
+        dirname.mkdir(parents=True, exist_ok=False)
+
+
+def read_json(fname):
+    fname = Path(fname)
+    with fname.open("rt") as handle:
+        return json.load(handle, object_hook=OrderedDict)
+
+
+def write_json(content, fname):
+    fname = Path(fname)
+    with fname.open("wt") as handle:
+        json.dump(content, handle, indent=4, sort_keys=False)
+
+
+def inf_loop(data_loader):
+    """wrapper function for endless data loader."""
+    for loader in repeat(data_loader):
+        yield from loader
+
+
+def flatten_batches(batches):
+    return np.concatenate([batch.numpy() for batch in batches], axis=0)
